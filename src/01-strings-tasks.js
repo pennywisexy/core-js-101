5ea1c7eba186facc58c8b58162502de9270aee06
Alexander Snitko <pennywisexy@gmail.com>
2020-12-09 15:21:14 +0300

    feat:made

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -302,10 +302,11 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (typeof value === 'string' || value instanceof (String)) {
+    return true;
+  } return false;
 }
-
 
 /**
  * Returns playid card id.

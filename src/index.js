4db1cc2ca7dc0678021690effdd77063f53c9df3
Alexandr Snitko <pennywisexe@gmail.com>
2020-12-10 13:02:22 +0300

    feat: made getLastDigit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// function getAngleBetweenVectors(x1, y1, x2, y2) {
-//   const angle = Math.acos((x1 * y1 + x2 * y2)
-//   / ((x1 ** 2 + y1 ** 2) ** (1 / 2) * (x2 ** 2 + y2 ** 2) ** (1 / 2)));
-//   return angle;
-// }
+// let a = 101;
 
-// console.log(getAngleBetweenVectors())
+// console.log(a)

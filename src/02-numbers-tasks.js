0a1e94f2a61f0fc8a673bbb1956e5d8c7f9f6a6d
Alexandr Snitko <pennywisexe@gmail.com>
2020-12-10 12:40:50 +0300

    getLinearEquationRoot

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -111,8 +111,6 @@ function getLinearEquationRoot(a, b) {
  */
 function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
   throw new Error('Not implemented');
-  // return Math.acos((x1 * y1 + x2 * y2)
-  // / ((x1 ** 2 + y1 ** 2) ** (1 / 2) * (x2 ** 2 + y2 ** 2) ** (1 / 2)));
 }
 
 /**

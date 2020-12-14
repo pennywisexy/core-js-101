425584304763beb5ef1aba4bf14c1512bd65147d
Alexander Snitko <pennywisexy@gmail.com>
2020-12-14 11:21:09 +0300

    feat:some:

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-// function toCsvText(arr) {
+// function propagateItemsByPositionIndex(arr) {
+//   let newArr = [];
+//   let count = 0;
+//   let count1 = 0;
 //   Array.prototype.map.call(arr, (e) => {
-//     e.concat('\n')
-//   }).join('');
-//   return arr
+//    if(arr.indexOf(e) === count) {
+//      Array.prototype.unshift.call(newArr, [])
+//      Array.prototype.map.call(newArr, (el) => {
+//        Array.prototype.push.call(el, e)
+//      });
+//      count += 1;
+//    }
+//    return newArr
+//   });
+//   Array.prototype.map.call(newArr, (elemArr) => {
+//     if(newArr.indexOf(elemArr) === count1){
+//       Array.prototype.map.call(elemArr, (element) => {
+//         console.log(element)
+
+//         element = arr[arr.indexOf(count1)]
+//       })
+//      count += 1;
+//     }
+//    return newArr
+//   })
+//   console.log(newArr)
 // }
-
-
-// console.log(toCsvText([[1],[2],[4]]))
+// console.log(propagateItemsByPositionIndex([1,2,3,4]))

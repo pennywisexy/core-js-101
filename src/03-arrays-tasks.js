f2d55956c91b2145652633c0a9cb465152bd92ba
Alexander Snitko <pennywisexy@gmail.com>
2020-12-14 17:24:33 +0300

    feat: sortCitiesArray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -431,8 +431,8 @@ function findAllOccurences(arr, item) {
  *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
  *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  return Array.prototype.toString.call(arr);
 }
 
 
@@ -462,8 +462,22 @@ function toStringList(/* arr */) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return Array.prototype.sort.call(arr, (a, b) => {
+    if (a.country > b.country) {
+      return 1;
+    }
+    if (a.country < b.country) {
+      return -1;
+    }
+    if (a.city > b.city) {
+      return 1;
+    }
+    if (a.city < b.city) {
+      return -1;
+    }
+    return 0;
+  });
 }
 
 /**
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-// function getFalsyValuesCount(arr) {
-//   console.log((arr))
-
-//   if(arr.length === 0) return false;
-//   return Array.prototype.filter.call(arr, (e) => {
-//     return Boolean(e) === false
-//   })
+// function getIdentityMatrix(n) {
+//   let arr = [];
+//   let matrix = [];
+//   arr.length = n
+//   arr.fill([0])
+//   console.log(arr)
+//   arr.flatMap((i, index) => {
+//     i.length = n;
+//     i.fill(0);
+//     i[index] = 1
+//     console.log(i, i[index], index)
+//   });
 // }
 
-// console.log(getFalsyValuesCount([1,,2]))
+// // function propagateItemsByPositionIndex(arr) {
+// //   return arr.flatMap((i, index) => Array.from({ length: index + 1 }, (v, k) => k).fill(i));
+// // }
+
+// console.log(getIdentityMatrix(4))
